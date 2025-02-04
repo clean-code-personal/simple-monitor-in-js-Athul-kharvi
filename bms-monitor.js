21e95eee6a4d4bebb0e944befa970bf720b8a2c1
athul <athulkharvi1234@gmail.com>
2025-02-04 14:21:39 +0530

    initializing the files

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,20 +1,34 @@
-function isOutOfRange(value, min, max, message) {
-  if (value < min || value > max) {
-    console.log(message);
-    return true;
-  }
-  return false;
+function checkTemperature(temperature) {
+  return temperature >= 0 && temperature <= 45;
+}
+
+function checkSoc(soc) {
+  return soc >= 20 && soc <= 80;
+}
+
+function checkChargeRate(chargeRate) {
+  return chargeRate <= 0.8;
 }
 
 function batteryIsOk(temperature, soc, chargeRate) {
-  return !(
-    isOutOfRange(temperature, 0, 45, 'Temperature is out of range!') ||
-    isOutOfRange(soc, 20, 80, 'State of Charge is out of range!') ||
-    isOutOfRange(chargeRate, 0, 0.8, 'Charge rate is out of range!')
-  );
-}
+  const isTemperatureOk = checkTemperature(temperature);
+  const isSocOk = checkSoc(soc);
+  const isChargeRateOk = checkChargeRate(chargeRate);
 
+  if (!isTemperatureOk) {
+    console.log('Temperature is out of range!');
+  }
+  if (!isSocOk) {
+    console.log('State of Charge is out of range!');
+  }
+  if (!isChargeRateOk) {
+    console.log('Charge rate is out of range!');
+  }
+
+  return isTemperatureOk && isSocOk && isChargeRateOk;
+}
 
 module.exports = {
   batteryIsOk,
 };
+s
